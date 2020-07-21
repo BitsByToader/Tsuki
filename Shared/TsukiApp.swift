c9a63089bdc69c5634852bf15be727b9133e0c23
Ifrim Tudor <ifrimtudor@protonmail.com>
2020-07-21 22:51:12 +0300

    Initial Commit

---
+++ Shared/TsukiApp.swift
@@ -0,0 +1,17 @@
+//
+//  TsukiApp.swift
+//  Shared
+//
+//  Created by Tudor Ifrim on 21/07/2020.
+//
+
+import SwiftUI
+
+@main
+struct TsukiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
